9db8d6b1322fc6b4a930659dc319c22182097216
Folha14 <josemiguel.folha@gmail.com>
2025-06-25 12:58:59 +0100

    Add files via upload

---
+++ LabEquip/Models/Conta.cs
@@ -0,0 +1,22 @@
+﻿namespace LabEquip.Models
+{
+    public class Conta
+    {
+        public Guid GuidConta { get; set; }
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NivelAcesso { get; set; }
+        public string Senha { get; set; }
+
+        public Conta()
+        {
+            GuidConta = Guid.NewGuid();
+            Id = GuidConta.ToString();
+            Nome = "";
+            Email = "";
+            NivelAcesso = 0;
+            Senha = "";
+        }
+    }
+}
---
+++ LabEquip/Program.cs
@@ -0,0 +1,32 @@
+using LabEquip.Models;
+
+namespace LabEquip
+{
+    public class Program
+    {
+        public static string Conetor = "";
+        public static string SmtpIP = "";
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSession(s => s.IdleTimeout = TimeSpan.FromMinutes(20));
+            builder.Services.AddMvc();
+
+            var config = builder.Configuration.GetSection("Configuracao").Get<Configuracao>();
+            Conetor = config.Conexao;
+            SmtpIP = config.SmtpIP;
+
+            var app = builder.Build();
+
+            app.UseSession();
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Equipamento}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
+    }
+}
