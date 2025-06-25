5293abfdffbe38d25361d5389a91bb11bd650732
Folha14 <josemiguel.folha@gmail.com>
2025-06-25 13:57:07 +0100

    Delete LabEquip directory

--- LabEquip/Models/Configuracao.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LabEquip.Models
-{
-    public class Configuracao
-    {
-        public string Conexao { get; set; }
-        public string SmtpIP { get; set; }
-        public string UsesTLS { get; set; }
-    }
-}
