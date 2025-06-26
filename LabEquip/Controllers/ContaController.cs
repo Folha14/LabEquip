2af4c7600811e283eff58dc8c6ad81900214beb0
Folha14 <josemiguel.folha@gmail.com>
2025-06-26 22:50:29 +0100

    Add files via upload

---
+++ LabEquip/Controllers/ContaController.cs
@@ -0,0 +1,29 @@
+﻿using LabEquip.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabEquip.Controllers
+{
+    public class ContaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Conta contaEnviada)
+        {
+            HelperConta helper = new HelperConta();
+            HttpContext.Session.SetString("contaAcesso", helper.serializeConta(helper.authUser(contaEnviada.email, contaEnviada.senha)));
+            return RedirectToAction("Index", "Equipamento");
+        }
+
+        public IActionResult Logout()
+        {
+            HelperConta helper = new HelperConta();
+            HttpContext.Session.SetString("contaAcesso", helper.serializeConta(helper.setGuest()));
+            return RedirectToAction("Index", "Equipamento");
+        }
+    }
+}
